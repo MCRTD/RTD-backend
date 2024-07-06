d28eb59cbb2704cb9da33a9dacbd4f9334425eca
phillychi3 <phillychi3@gmail.com>
2024-07-06 17:10:06 +0800

    feat: edit cors url

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 
 	corsconfig := cors.DefaultConfig()
 	corsconfig.AllowCredentials = true
-	corsconfig.AllowOrigins = []string{"http://localhost:5173", "http://localhost:8888"}
+	corsconfig.AllowOrigins = []string{"http://localhost:5173", "http://localhost:8888", "https://rtdweb.zeabur.app"}
 	router.Use(cors.New(
 		corsconfig,
 	))
